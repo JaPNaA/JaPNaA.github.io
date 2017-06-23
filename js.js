8a8bac147cf8778fc5b773562db3021c4f459b3e
JaPNaA <leonehuang100@gmail.com>
2017-06-23 19:36:35 -0400

    Massive changes.
    Removed Everything
    Added Everything.

--- js.js
+++ js.js
@@ -1,32 +1,11 @@
-document.getElementById("testJS").innerHTML = "JS is loaded.";
-(function() { //Remove this later on.
-    var f = '__';
-    addEventListener('keydown', function(e) {
-        f += e.key;
-        f = f.substr(1, 2);
-        if (f.toLowerCase() == "bd" && new Date().toLocaleDateString() ==
-            "6/23/2017") {
-            alert(
-                "And Happy Birthday, Aryan.\nDouble-click to go fullscreen"
-            );
-            document.body.innerHTML =
-                "<fsd>HOOPY BARTHDEE, Aryan!<div>--from Leone</div></fsd>";
-            document.body.classList.add('bda');
-            addEventListener('resize', function() {
-                document.body.style.lineHeight = document.body
-                    .clientHeight +
-                    "px";
-            }, false);
-            addEventListener('dblclick', function() {
-                $("body").webkitRequestFullscreen();
-            }, false);
-            document.body.style.lineHeight = document.body.clientHeight +
-                "px";
+(function() {
+    addEventListener('wheel', function(e) {
+        if(e.deltaY<0){
+            console.log("Scroll up");
+            $("#head").style.top="0px";
+        } else {
+            console.log("Scroll down");
+            $("#head").style.top="-64px";
         }
     }, false);
 }());
-
-setInterval(() => {
-    $('#dots').innerHTML += ".";
-    $("#dotSc").scrollIntoViewIfNeeded();
-}, 45);
