3763a4931259c2e0ef2f4ca19042a6498fca4096
JaPNaA <leonehuang100@gmail.com>
2017-06-23 18:23:01 -0400

    Change little stuff for pettiness

--- js.js
+++ js.js
@@ -1,26 +1,32 @@
 document.getElementById("testJS").innerHTML = "JS is loaded.";
-var f = '__';
-addEventListener('keydown', function(e) {
-    f += e.key;
-    f = f.substr(1, 2);
-    if (f.toLowerCase() == "bd" && new Date().toLocaleDateString() ==
-        "6/23/2017") {
-        alert("And Happy Birthday, Aryan.\nDouble-click to go fullscreen");
-        document.body.innerHTML = "<fsd>HOOPY BARTHDEE, Aryan!<div>--from Leone</div></fsd>";
-        document.body.classList.add('bda');
-        addEventListener('resize', function() {
+(function() { //Remove this later on.
+    var f = '__';
+    addEventListener('keydown', function(e) {
+        f += e.key;
+        f = f.substr(1, 2);
+        if (f.toLowerCase() == "bd" && new Date().toLocaleDateString() ==
+            "6/23/2017") {
+            alert(
+                "And Happy Birthday, Aryan.\nDouble-click to go fullscreen"
+            );
+            document.body.innerHTML =
+                "<fsd>HOOPY BARTHDEE, Aryan!<div>--from Leone</div></fsd>";
+            document.body.classList.add('bda');
+            addEventListener('resize', function() {
+                document.body.style.lineHeight = document.body
+                    .clientHeight +
+                    "px";
+            }, false);
+            addEventListener('dblclick', function() {
+                $("body").webkitRequestFullscreen();
+            }, false);
             document.body.style.lineHeight = document.body.clientHeight +
                 "px";
-        }, false);
-        addEventListener('dblclick', function() {
-            $("body").webkitRequestFullscreen();
-        }, false);
-        document.body.style.lineHeight = document.body.clientHeight +
-            "px";
-    }
-}, false);
+        }
+    }, false);
+}());
 
-setInterval(()=>{
-    $('#dots').innerHTML+=".";
+setInterval(() => {
+    $('#dots').innerHTML += ".";
     $("#dotSc").scrollIntoViewIfNeeded();
-},45);
+}, 45);
