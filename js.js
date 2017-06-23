407507e406950c586ec8bd30f94edc5a4ee8c929
JaPNaA <leonehuang100@gmail.com>
2017-06-23 17:11:27 -0400

    updated things and added things

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
 document.getElementById("testJS").innerHTML="JS is loaded.";
+var f='';
+addEventListener('keydown', function(e){
+    f+=e.key;
+    f=f.substr(0,2);
+    if(f.toLowerCase()=="bd"&&new Date().toLocaleDateString()=="6/23/2017"){
+        alert("And Happy Birthday, Aryan.");
+        document.body.innerHTML="HOOPY BARTHDEE!";
+    }
+})
