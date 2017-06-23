b5ecd36961a9da69897c1e5e0d22d4988935ca58
JaPNaA <leonehuang100@gmail.com>
2017-06-23 17:45:45 -0400

    added temp stuff for people

--- js.js
+++ js.js
@@ -1,10 +1,21 @@
-document.getElementById("testJS").innerHTML="JS is loaded.";
-var f='';
-addEventListener('keydown', function(e){
-    f+=e.key;
-    f=f.substr(0,2);
-    if(f.toLowerCase()=="bd"&&new Date().toLocaleDateString()=="6/23/2017"){
-        alert("And Happy Birthday, Aryan.");
-        document.body.innerHTML="HOOPY BARTHDEE!";
+document.getElementById("testJS").innerHTML = "JS is loaded.";
+var f = '__';
+addEventListener('keydown', function(e) {
+    f += e.key;
+    f = f.substr(1, 2);
+    if (f.toLowerCase() == "bd" && new Date().toLocaleDateString() ==
+        "6/23/2017") {
+        alert("And Happy Birthday, Aryan.\nDouble-click to go fullscreen");
+        document.body.innerHTML = "<fsd>HOOPY BARTHDEE, Aryan!<div>--from Leone</div></fsd>";
+        document.body.classList.add('bda');
+        addEventListener('resize', function() {
+            document.body.style.lineHeight = document.body.clientHeight +
+                "px";
+        }, false);
+        addEventListener('dblclick', function() {
+            $("body").webkitRequestFullscreen();
+        }, false);
+        document.body.style.lineHeight = document.body.clientHeight +
+            "px";
     }
-})
+}, false);
