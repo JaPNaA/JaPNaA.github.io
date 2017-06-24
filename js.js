1a9ba8c4c1fa3c72625e377741a0d736f2274a3a
JaPNaA <leonehuang100@gmail.com>
2017-06-24 11:10:02 -0400

    Created basic layout of site

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
 (function() {
-    addEventListener('wheel', function(e) {
-        if(e.deltaY<0){
-            console.log("Scroll up");
-            $("#head").style.top="0px";
-        } else {
-            console.log("Scroll down");
-            $("#head").style.top="-64px";
+    addEventListener('scroll', function(e) {
+        if ($("body").scrollTop < 1) {
+            $("#head").style.position = "absolute";
+            $("#head").style.top = "";
         }
     }, false);
+    addEventListener('wheel', function(e) {
+        if (e.deltaY < 0) {
+            $("#head").style.top = "0px";
+        } else {
+            $("#head").style.top = "-64px";
+        }
+        $("#head").style.position = "fixed";
+    }, {passive: true, capture:false});
 }());
