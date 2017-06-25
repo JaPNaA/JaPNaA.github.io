7e9436539ff7050395632ca7c045c04c3244e614
JaPNaA <leonehuang100@gmail.com>
2017-06-25 12:25:45 -0400

    added loading from ext. file

--- js.js
+++ js.js
@@ -12,15 +12,34 @@
             $("#head").style.top = "-64px";
         }
         $("#head").style.position = "fixed";
-    }, {passive: true, capture:false});
-    $('.JaPNaAT').addEventListener('auxclick', function(){
-        this.setAttribute('contentEditable',!0);
+    }, {
+        passive: true,
+        capture: false
+    });
+    $('.JaPNaAT').addEventListener('auxclick', function() {
+        this.setAttribute('contentEditable', !0);
         this.focus();
     }, true);
-    $('.JaPNaAT').addEventListener('contextmenu', function(e){
+    $('.JaPNaAT').addEventListener('contextmenu', function(e) {
         e.preventDefault();
     }, true);
     $('.JaPNaAT').addEventListener('blur', function() {
         this.removeAttribute('contentEditable');
     }, false);
+}());
+
+function getFile(e, c) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            c(this.responseText);
+        }
+    };
+    xhttp.open("GET", e, true);
+    xhttp.send();
+}
+
+(function() {
+    $("#content").innerHTML = "Loading content..."
+    $("#content").classList.add('loading');
 }());
