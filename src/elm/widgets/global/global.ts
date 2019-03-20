b09645634e6eae8d0932e664516966536b175c13
JaPNaA <leonehuang100@gmail.com>
2019-03-20 19:47:57 -0400

    add hamburger svg

--- src/elm/widgets/global/global.ts
+++ src/elm/widgets/global/global.ts
@@ -1,18 +1,26 @@
 import Widget from "../widget";
+import SiteResources from "../../../siteResources";
+import SiteConfig from "../../../siteConfig";
+import App from "../../../app";
 
 class GlobalWidget extends Widget {
     public widgetName: string = "GlobalWidget";
     protected elm: HTMLDivElement;
+    private app: App;
 
-    constructor() {
+    constructor(app: App) {
         super();
+        this.app = app;
 
         this.elm = document.createElement("div");
     }
 
-    // public setup(): void {
-    //     super.setup();
-    // }
+    public setup(): void {
+        const img = SiteResources.loadImage(SiteConfig.paths.hamburger).image;
+        this.elm.appendChild(img);
+        super.setup();
+        // this.app.onViewChange(() => console.log("vc"));
+    }
 }
 
 export default GlobalWidget;
