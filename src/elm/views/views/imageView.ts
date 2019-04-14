a1b06880b9258de430e9705acf4cb42af1ae66ba
JaPNaA <leonehuang100@gmail.com>
2019-04-14 11:34:57 -0400

    implement flick panning

--- src/elm/views/views/imageView.ts
+++ src/elm/views/views/imageView.ts
@@ -17,12 +17,21 @@ class ImageView extends View {
     private image?: HTMLImageElement;
     private closeButton: HTMLImageElement;
 
+    private src?: string;
+
     private width: number;
     private height: number;
 
     private x: number;
+    private lastX: number;
+    private targetX: number;
+    private velocityX: number;
     private y: number;
+    private lastY: number;
+    private targetY: number;
+    private velocityY: number;
     private scale: number;
+    private tscale: number;
     private dragging: boolean;
     private userAdjusted: boolean;
 
@@ -31,29 +40,35 @@ class ImageView extends View {
         this.elm = document.createElement("div");
         this.canvas = document.createElement("canvas");
         this.closeButton = SiteResources.loadImage(SiteConfig.path.img.close).image;
+        this.X = this.getX();
 
-        const X = this.canvas.getContext("2d");
-        if (!X) { throw new Error("Canvas not supported"); }
-        this.X = X;
-
-        this.elm.appendChild(this.canvas);
-
-        this.x = 0;
-        this.y = 0;
         this.width = 0;
         this.height = 0;
-        this.scale = 1;
+
+        this.lastX = this.targetX = this.x = 0;
+        this.lastY = this.targetY = this.y = 0;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.tscale = this.scale = 1;
+
         this.dragging = false;
         this.userAdjusted = false;
 
-        // this.background = this.createBackground();
-        if (stateData) {
-            this.setImageSrc(stateData);
-        }
+        this.src = stateData;
+    }
+
+    public async setup(): Promise<void> {
+        super.setup();
+        this.elm.appendChild(this.canvas);
+
         this.reqanfLoop();
         this.addEventHandlers();
         this.resizeHandler();
         this.resetImagePosition();
+
+        if (this.src) {
+            this.setImageSrc(this.src);
+        }
     }
 
     public setImageSrc(src: string) {
@@ -63,12 +78,37 @@ class ImageView extends View {
             );
     }
 
+
     private setImage(image: HTMLImageElement) {
         this.image = image;
         this.resetImagePosition();
+        this.stopAnimations();
+    }
+
+    private getX(): CanvasRenderingContext2D {
+        const X = this.canvas.getContext("2d");
+        if (!X) { throw new Error("Canvas not supported"); }
+        return X;
     }
 
     private reqanfLoop(): void {
+        this.x += (this.targetX - this.x) / 10;
+        this.y += (this.targetY - this.y) / 10;
+        this.scale += (this.tscale - this.scale) / 10;
+        if (this.dragging) {
+            this.velocityX = (this.x - this.lastX) * 0.7 + this.velocityX * 0.3;
+            this.velocityY = (this.y - this.lastY) * 0.7 + this.velocityY * 0.3;
+            this.lastX = this.x;
+            this.lastY = this.y;
+        } else {
+            this.velocityX *= 0.93;
+            this.velocityY *= 0.93;
+            this.x += this.velocityX;
+            this.y += this.velocityY;
+            this.targetX += this.velocityX;
+            this.targetY += this.velocityY;
+        }
+
         this.draw();
         requestAnimationFrame(this.reqanfLoop.bind(this));
     }
@@ -81,6 +121,11 @@ class ImageView extends View {
             0, 0, this.image.width, this.image.height,
             this.x, this.y, this.image.width * this.scale, this.image.height * this.scale
         );
+        // this.X.drawImage(
+        //     this.image,
+        //     0, 0, this.image.width, this.image.height,
+        //     this.tx, this.ty, this.image.width * this.tscale, this.image.height * this.tscale
+        // );
         this.X.drawImage(this.closeButton, 0, 0);
     }
 
@@ -105,8 +150,12 @@ class ImageView extends View {
     }
 
     private resizeHandler(): void {
-        this.x += (this.app.width - this.width) / 2;
-        this.y += (this.app.height - this.height) / 2;
+        const dx = (this.app.width - this.width) / 2;
+        const dy = (this.app.height - this.height) / 2;
+        this.x += dx;
+        this.y += dy;
+        this.targetX += dx;
+        this.targetY += dy;
 
         this.width = this.canvas.width = this.app.width;
         this.height = this.canvas.height = this.app.height;
@@ -124,15 +173,17 @@ class ImageView extends View {
             factor = 1 / factor;
         }
 
-        this.scale *= factor;
+        this.tscale *= factor;
 
-        this.x -= (e.clientX - this.x) * (factor - 1);
-        this.y -= (e.clientY - this.y) * (factor - 1);
+        this.targetX -= (e.clientX - this.targetX) * (factor - 1);
+        this.targetY -= (e.clientY - this.targetY) * (factor - 1);
         this.userAdjusted = true;
     }
 
     private mouseMoveHandler(e: MouseEvent): void {
         if (!this.dragging) { return; }
+        this.targetX += e.movementX;
+        this.targetY += e.movementY;
         this.x += e.movementX;
         this.y += e.movementY;
         this.userAdjusted = true;
@@ -146,7 +197,6 @@ class ImageView extends View {
         this.dragging = false;
     }
 
-
     private keyDownHandler(e: KeyboardEvent): void {
         if (e.keyCode === 48) {
             this.resetImagePosition();
@@ -156,18 +206,24 @@ class ImageView extends View {
     private resetImagePosition(): void {
         if (!this.image) { return; }
         if (this.image.width <= this.width && this.image.height <= this.height) {
-            this.scale = 1;
+            this.tscale = 1;
         } else {
-            this.scale = Math.min(
+            this.tscale = Math.min(
                 this.width / this.image.width,
                 this.height / this.image.height
             );
         }
 
-        this.x = (this.width - this.image.width * this.scale) / 2;
-        this.y = (this.height - this.image.height * this.scale) / 2;
+        this.targetX = (this.width - this.image.width * this.tscale) / 2;
+        this.targetY = (this.height - this.image.height * this.tscale) / 2;
 
         this.userAdjusted = false;
+    }
+
+    private stopAnimations(): void {
+        this.x = this.targetX;
+        this.y = this.targetY;
+        this.scale = this.tscale;
     }
 }
 
