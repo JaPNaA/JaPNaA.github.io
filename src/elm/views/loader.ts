851389271003bd49c112fbd5e41cc3ab66e663b0
JaPNaA <leonehuang100@gmail.com>
2019-04-20 13:06:29 -0400

    update view loader

--- src/elm/views/loader.ts
+++ src/elm/views/loader.ts
@@ -1,4 +1,8 @@
+import "./views/imageView/imageView";
 import "./views/404";
+import "./views/allThingies";
+import "./views/frameView";
+import "./views/menu";
 import "./views/overview";
 import "./views/projectInfo";
 import "./views/splashScreen";
