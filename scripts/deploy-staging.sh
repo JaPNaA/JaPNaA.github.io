07cfa137fb1d81a63e7bea431f5e19dc22e926df
JaPNaA <leonehuang100@gmail.com>
2019-04-29 15:40:33 -0400

    add deploy-staging script

---
+++ scripts/deploy-staging.sh
@@ -0,0 +1,27 @@
+set -e
+
+# clear build directory
+if [ -d "./build" ]
+then
+    rm build -r
+fi
+mkdir build
+
+# build
+npm run build
+cp -r docs/* build
+
+# do the git stuff (Adapted from Microsoft/monaco-editor)
+COMMITTER_USER_NAME="$(git log --format='%an' -1)"
+COMMITTER_EMAIL="$(git log --format='%ae' -1)"
+BRANCH_NAME="staging"
+
+cd build
+git init
+git config user.name "${COMMITTER_USER_NAME}"
+git config user.email "${COMMITTER_EMAIL}"
+git remote add origin "git@github.com:${COMMITTER_USER_NAME}/JaPNaA.github.io.git"
+git checkout -b "${BRANCH_NAME}"
+git add .
+git commit -m "Stage site"
+git push origin "${BRANCH_NAME}" --force
