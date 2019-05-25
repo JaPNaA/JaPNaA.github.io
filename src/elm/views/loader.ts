6098271d8f963df5daf405c565f48f2fd6174f8c
JaPNaA <leonehuang100@gmail.com>
2019-05-24 21:17:40 -0400

    dynamically import views

--- src/elm/views/loader.ts
+++ src/elm/views/loader.ts
@@ -1,10 +1,15 @@
 import "./imageView/imageView";
 import "./404/404";
-import "./allThingies/allThingies";
-import "./frameView/frameView";
-import "./menu/menu";
-import "./overview/overview";
-import "./projectInfo/projectInfo";
-import "./shortUrl/shortUrl";
-import "./test/test";
 import "./splashScreen/splashScreen";
+import ViewMap from "../../core/view/viewMap";
+import ViewClassGhost from "../../core/view/viewClassGhost";
+
+ViewMap.add(new ViewClassGhost("imageView", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("404", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("allThingies", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("frameView", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("menu", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("overview", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("projectInfo", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("shortUrl", () => import("./imageView/imageView")));
+ViewMap.add(new ViewClassGhost("test", () => import("./imageView/imageView")));
