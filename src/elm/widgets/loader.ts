48471fc5f5fb2b03bc3c7cca9e45e135d78a85d1
JaPNaA <leonehuang100@gmail.com>
2019-05-25 13:55:19 -0400

    lazy-load widgets

--- src/elm/widgets/loader.ts
+++ src/elm/widgets/loader.ts
@@ -1,5 +1,9 @@
 import "./global/global";
-import "./hexagonsTitle/hexagonsTitle";
-import "./iframe/iframe";
-import "./latestProjects/latestProjects";
-import "./stickyBar/stickyBar";
+
+import WidgetMap from "../../core/widget/widgetMap";
+import WidgetClassGhost from "../../core/widget/widgetClassGhost";
+
+WidgetMap.add(new WidgetClassGhost("hexagonsTitle", () => import("./hexagonsTitle/hexagonsTitle")));
+WidgetMap.add(new WidgetClassGhost("iframe", () => import("./iframe/iframe")));
+WidgetMap.add(new WidgetClassGhost("latestProjects", () => import("./latestProjects/latestProjects")));
+WidgetMap.add(new WidgetClassGhost("stickyBar", () => import("./stickyBar/stickyBar")));
