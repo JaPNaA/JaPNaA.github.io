9800260c6d2a0c11d1c36e8440e476f60108847d
JaPNaA <leonehuang100@gmail.com>
2019-06-08 17:59:52 -0400

    dynamically set the public path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import App from "./app";
 
+// @ts-ignore
+__webpack_public_path__ = location.href.slice(0, location.href.lastIndexOf("/") + 1);
+
 const app = new App();
 app.setup();
 
 console.log(app);
-
-// import "./core/app/mainApp";
