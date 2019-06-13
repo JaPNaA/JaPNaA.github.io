b98cf82c72a5608bafe9a568eda415ab3413ddcc
JaPNaA <leonehuang100@gmail.com>
2019-06-12 22:08:04 -0400

    update isProjectCard to handle null and undefined

--- src/utils/isProjectCard.ts
+++ src/utils/isProjectCard.ts
@@ -2,5 +2,5 @@ import IProject from "../types/project/project";
 import ICard from "../types/project/card";
 
 export default function isProjectCard(proj: IProject): proj is ICard {
-    return proj.type === "card";
+    return proj && proj.type === "card";
 }
