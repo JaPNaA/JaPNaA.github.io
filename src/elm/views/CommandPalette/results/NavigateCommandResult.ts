e3926250b65fd80124977106d79ea9b4765b1585
JaPNaA <leonehuang100@gmail.com>
2019-09-16 19:03:27 -0400

    fix bugs

--- src/elm/views/CommandPalette/results/NavigateCommandResult.ts
+++ src/elm/views/CommandPalette/results/NavigateCommandResult.ts
@@ -2,6 +2,7 @@ import CommandResult from "../CommandResult";
 import IApp from "../../../../core/types/app/IApp";
 import parseAppStateURL from "../../../../core/utils/parseAppStateURL";
 import resolveUrl from "../../../../utils/resolveUrl";
+import siteConfig from "../../../../SiteConfig";
 
 class NavigateCommandResult extends CommandResult {
     private to: string;
@@ -21,10 +22,17 @@ class NavigateCommandResult extends CommandResult {
     public activate(app: IApp) {
         const state = parseAppStateURL(this.to);
 
-        if (!state) {
+        if (!state || !this.to.startsWith(siteConfig.path.base)) {
             location.assign(this.to);
         } else {
-            app.views.switchAndInit(state.viewName, state);
+            app.views.createAndSetupViewWithFallbacks(state.viewName, state)
+                .then(viewWithFallbackStatus => {
+                    if (viewWithFallbackStatus.isFallback) {
+                        location.assign(this.to);
+                    } else {
+                        app.views.switch(viewWithFallbackStatus.view);
+                    }
+                });
         }
     }
 }
