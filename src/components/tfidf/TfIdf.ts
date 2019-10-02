85cd5b1078f59047dd3329e879090ecccdb9611b
JaPNaA <leonehuang100@gmail.com>
2019-10-01 22:19:08 -0400

    partial matching last tokens

--- src/components/tfidf/TfIdf.ts
+++ src/components/tfidf/TfIdf.ts
@@ -1,7 +1,11 @@
+import looseStartsWith from "../../utils/looseStartsWith";
+
 class TfIdf<T extends number | string> {
     private static isStopWordMap: { [x: string]: boolean } = { "a": true, "about": true, "above": true, "after": true, "again": true, "against": true, "all": true, "am": true, "an": true, "and": true, "any": true, "are": true, "as": true, "at": true, "be": true, "because": true, "been": true, "before": true, "being": true, "below": true, "between": true, "both": true, "but": true, "by": true, "can": true, "did": true, "do": true, "does": true, "doing": true, "don": true, "down": true, "during": true, "each": true, "few": true, "for": true, "from": true, "further": true, "had": true, "has": true, "have": true, "having": true, "he": true, "her": true, "here": true, "hers": true, "herself": true, "him": true, "himself": true, "his": true, "how": true, "i": true, "if": true, "in": true, "into": true, "is": true, "it": true, "its": true, "itself": true, "just": true, "me": true, "more": true, "most": true, "my": true, "myself": true, "no": true, "nor": true, "not": true, "now": true, "of": true, "off": true, "on": true, "once": true, "only": true, "or": true, "other": true, "our": true, "ours": true, "ourselves": true, "out": true, "over": true, "own": true, "s": true, "same": true, "she": true, "should": true, "so": true, "some": true, "such": true, "t": true, "than": true, "that": true, "the": true, "their": true, "theirs": true, "them": true, "themselves": true, "then": true, "there": true, "these": true, "they": true, "this": true, "those": true, "through": true, "to": true, "too": true, "under": true, "until": true, "up": true, "very": true, "was": true, "we": true, "were": true, "what": true, "when": true, "where": true, "which": true, "while": true, "who": true, "whom": true, "why": true, "will": true, "with": true, "you": true, "your": true, "yours": true, "yourself": true, "yourselves": true };
+    private static looseMatchScoreCutoff = 8;
     private allWords: Map<string, number>;
     private documents: [T, Map<string, number>][];
+
 
     constructor() {
         this.documents = [];
@@ -44,12 +48,16 @@ class TfIdf<T extends number | string> {
 
     public query(tokens: string | string[]): T[] {
         const tokensArr: string[] = TfIdf.stringOrArrayToTokens(tokens);
+        const lastToken = tokensArr.pop();
 
         const tokensLength = tokensArr.length;
         const results: [T, number][] = [];
 
         for (const [id, index] of this.documents) {
-            const score = this.calcScore(tokensArr, tokensLength, index);
+            let score = this.calcScore(tokensArr, tokensLength, index);
+            if (lastToken) {
+                console.log(score += this.calcPartialTokenScore(lastToken, index));
+            }
 
             if (score > 0.1) {
                 results.push([id, score]);
@@ -60,7 +68,7 @@ class TfIdf<T extends number | string> {
         return results.map(e => e[0]);
     }
 
-    public calcScore(tokens: string[], tokensLength: number, doc: Map<string, number>): number {
+    private calcScore(tokens: string[], tokensLength: number, doc: Map<string, number>): number {
         let score = 0;
 
         for (const token of tokens) {
@@ -68,6 +76,19 @@ class TfIdf<T extends number | string> {
             if (!val) { continue; }
             const freq = this.allWords.get(token)!;
             score += val / freq / tokensLength;
+        }
+
+        return score;
+    }
+
+    private calcPartialTokenScore(token: string, doc: Map<string, number>): number {
+        let score = 0;
+
+        for (const [key, val] of doc) {
+            const matchScore = looseStartsWith(token, key);
+            if (matchScore < 0 || matchScore > TfIdf.looseMatchScoreCutoff) { continue; }
+            const freq = this.allWords.get(key)!;
+            score += val / freq / (matchScore + 1);
         }
 
         return score;
