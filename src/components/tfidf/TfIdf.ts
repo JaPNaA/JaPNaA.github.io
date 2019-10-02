bdece91bfebe8eb017b1d07897e5205f5ce81527
JaPNaA <leonehuang100@gmail.com>
2019-10-02 18:06:45 -0400

    accept any type

--- src/components/tfidf/TfIdf.ts
+++ src/components/tfidf/TfIdf.ts
@@ -1,6 +1,6 @@
 import looseStartsWith from "../../utils/looseStartsWith";
 
-class TfIdf<T extends number | string> {
+class TfIdf<T> {
     private static isStopWordMap: { [x: string]: boolean } = { "a": true, "about": true, "above": true, "after": true, "again": true, "against": true, "all": true, "am": true, "an": true, "and": true, "any": true, "are": true, "as": true, "at": true, "be": true, "because": true, "been": true, "before": true, "being": true, "below": true, "between": true, "both": true, "but": true, "by": true, "can": true, "did": true, "do": true, "does": true, "doing": true, "don": true, "down": true, "during": true, "each": true, "few": true, "for": true, "from": true, "further": true, "had": true, "has": true, "have": true, "having": true, "he": true, "her": true, "here": true, "hers": true, "herself": true, "him": true, "himself": true, "his": true, "how": true, "i": true, "if": true, "in": true, "into": true, "is": true, "it": true, "its": true, "itself": true, "just": true, "me": true, "more": true, "most": true, "my": true, "myself": true, "no": true, "nor": true, "not": true, "now": true, "of": true, "off": true, "on": true, "once": true, "only": true, "or": true, "other": true, "our": true, "ours": true, "ourselves": true, "out": true, "over": true, "own": true, "s": true, "same": true, "she": true, "should": true, "so": true, "some": true, "such": true, "t": true, "than": true, "that": true, "the": true, "their": true, "theirs": true, "them": true, "themselves": true, "then": true, "there": true, "these": true, "they": true, "this": true, "those": true, "through": true, "to": true, "too": true, "under": true, "until": true, "up": true, "very": true, "was": true, "we": true, "were": true, "what": true, "when": true, "where": true, "which": true, "while": true, "who": true, "whom": true, "why": true, "will": true, "with": true, "you": true, "your": true, "yours": true, "yourself": true, "yourselves": true };
     private static looseMatchScoreCutoff = 8;
     private allWords: Map<string, number>;
@@ -56,7 +56,7 @@ class TfIdf<T extends number | string> {
         for (const [id, index] of this.documents) {
             let score = this.calcScore(tokensArr, tokensLength, index);
             if (lastToken) {
-                console.log(score += this.calcPartialTokenScore(lastToken, index));
+                score += this.calcPartialTokenScore(lastToken, index);
             }
 
             if (score > 0.1) {
