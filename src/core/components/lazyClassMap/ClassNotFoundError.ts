ef30457231f5dcee69361e96525341a656f5b8e4
JaPNaA <leonehuang100@gmail.com>
2019-10-20 17:55:41 -0400

    [core] remove viewmap

--- src/core/components/lazyClassMap/ClassNotFoundError.ts
+++
@@ -1,7 +0,0 @@
-import CustomError from "../../types/CustomError";
-
-export default class ClassNotFoundError extends CustomError {
-    constructor(className: string) {
-        super("Class with name \"" + className + "\" doesn't exist, tried importing");
-    }
-}
--- src/core/components/lazyClassMap/NoImporterError.ts
+++
@@ -1,7 +0,0 @@
-import CustomError from "../../types/CustomError";
-
-export default class NoImporterError extends CustomError {
-    constructor() {
-        super("Tried to import class without importer function");
-    }
-}
